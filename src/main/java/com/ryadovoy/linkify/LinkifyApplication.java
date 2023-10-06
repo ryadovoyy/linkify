e99e136ab54db0874fe2ef650fe3416a2382ed2e
ryadovoyy <holstinindvs@gmail.com>
2023-10-06 16:23:50 +0700

    initial commit

---
+++ src/main/java/com/ryadovoy/linkify/LinkifyApplication.java
@@ -0,0 +1,11 @@
+package com.ryadovoy.linkify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LinkifyApplication.class, args);
+    }
+}
